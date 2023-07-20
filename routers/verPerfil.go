5aaede162a7c001470bd9161e1d19a2730ada856
Jose Egas <109235867+joseegas@users.noreply.github.com>
2023-07-20 17:00:25 -0400

    ver perfil

---
+++ routers/verPerfil.go
@@ -0,0 +1,40 @@
+package routers
+
+import (
+	"encoding/json"
+	"fmt"
+	"twitterGo/bd"
+	"twitterGo/models"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func VerPerfil(request events.APIGatewayProxyRequest) models.RespApi{
+	var r models.RespApi
+	r.Status=400
+
+	fmt.Println("Entré en VerPerfil")
+	ID:=request.QueryStringParameters["id"]
+	if len(ID)<1{
+		r.Message="El parámetro ID es obligatorio"
+		return r
+	}
+
+	perfil, err:= bd.BuscoPerfil(ID)
+	if err!= nil{
+		r.Message="Ocurrió un error al intentar buscar el registro"+err.Error()
+		return r
+	}
+
+	respJson,err := json.Marshal(perfil)
+	if err!=nil{
+		r.Status=500
+		r.Message="Error al formatear los datos del usuario como JSON "+err.Error()
+		return r
+	}
+
+	r.Status=200
+	r.Message=string(respJson)
+	return r
+
+}
