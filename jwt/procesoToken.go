a3561f78db5ccbcbb477d23f22b59db66e6eb4db
Jose Egas <109235867+joseegas@users.noreply.github.com>
2023-07-20 17:33:47 -0400

    correccion validación token

--- jwt/procesoToken.go
+++ jwt/procesoToken.go
@@ -3,6 +3,7 @@ package jwt
 import (
 	"errors"
 	"strings"
+	"twitterGo/bd"
 	"twitterGo/models"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -25,8 +26,16 @@ func ProcesoToken(tk string, JWTSign string)(*models.Claim, bool, string, error)
 	tkn,err:=jwt.ParseWithClaims(tk, &claims, func(token *jwt.Token)(interface{},error){
 		return miClave,nil
 	})
-	if err !=nil{
+	
+	if err ==nil{
 		//Rutina que chequea contra la BD
+		_,encontrado,_:=bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado{
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		
+		return &claims, encontrado,IDUsuario,nil
 	}
 
 	if !tkn.Valid{
